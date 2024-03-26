aa40b57fd53018b514158d4e8d9dff21c8e85c96
M Adnan Khan <160040993+Adnan00Khan@users.noreply.github.com>
2024-03-27 01:49:42 +0500

    add

---
+++ typescript/project01_number_guessing-game/index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+
+let randomNumber= Math.floor(Math.random() * 10 +1);
+console.log(randomNumber)
+let ans = await inquirer.prompt([{
+
+    name:"game",
+    message:"please guess a number between 1-10 :",
+    type:"number"
+
+}]);
+if (ans.game === randomNumber) {
+    console.log("you guess the right number")
+}
+else{
+    console.log("wrong answer")
+}
--- typescript/step00_helloworld/app.ts
+++ typescript/step00_helloworld/app.ts
@@ -2,3 +2,8 @@ let message ="hello world";
 console.log(message);
 var msg = "thank you governor kamran khan tassori";
 console.log(msg);
+for (let i=1 ;i<5 ; i++){
+
+    if (i===3) continue;
+    console.log(i);
+}
