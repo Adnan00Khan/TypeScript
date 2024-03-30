0391d9fe25939ac3ff6aa8199194ced674e92692
M Adnan Khan <160040993+Adnan00Khan@users.noreply.github.com>
2024-03-30 14:17:42 +0500

    add

--- typescript/TypeScript 45 Questions/018/Q18.js
+++ typescript/TypeScript 45 Questions/018/Q18.js
@@ -1,27 +1,16 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 // Q18
 // Seeing the World
 var places = ["Paris", "London", "Bali", "Dubai", "New York City"];
-console.log(places);
-var alphabetical_order = __spreadArray([], places, true).sort();
-console.log(alphabetical_order);
-console.log(places);
-var reverse_alphabetical_order = __spreadArray([], places, true).sort().reverse();
-console.log(reverse_alphabetical_order);
-console.log(places);
+console.log(" original  : " + places);
+console.log(" alphabetical order  : " + places.slice().sort());
+console.log(" original  : " + places);
+console.log("  reverse alphabetical order  : " + places.slice().sort().reverse());
+console.log(" original  : " + places);
 places.reverse();
-console.log(places);
+console.log("Reverse order  : " + places);
 places.reverse();
-console.log(places);
+console.log(" original  : " + places);
 places.sort();
-console.log(places);
+console.log(" stored in alphabetical order  : " + places);
 places.sort().reverse();
-console.log(places);
+console.log(" stored in reverse alphabetical order  : " + places);
--- typescript/TypeScript 45 Questions/018/Q18.ts
+++ typescript/TypeScript 45 Questions/018/Q18.ts
@@ -1,25 +1,29 @@
 // Q18
 // Seeing the World
+
+   
+
 var places=["Paris","London","Bali","Dubai","New York City"];
- console.log(places);
 
- var  alphabetical_order =[...places].sort();
- console.log(alphabetical_order);
- console.log(places);
+ console.log(" original  : " + places);
+ 
 
- var  reverse_alphabetical_order =[...places].sort().reverse();
- console.log(reverse_alphabetical_order);
- console.log(places);
+ 
+ console.log(" alphabetical order  : " + places.slice().sort());
+ console.log(" original  : " + places);
 
- places.reverse();
- console.log(places);
+ console.log("  reverse alphabetical order  : " +places.slice().sort().reverse());
+ console.log(" original  : " + places);
 
  places.reverse();
- console.log(places);
+ console.log("Reverse order  : " +places);
+
+ places.reverse();
+ console.log(" original  : " +places);
 
  places.sort();
- console.log(places);
+ console.log(" stored in alphabetical order  : " +places);
 
  places.sort().reverse();
- console.log(places);
+ console.log(" stored in reverse alphabetical order  : " +places);
 
